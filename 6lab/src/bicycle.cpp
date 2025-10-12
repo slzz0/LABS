4907dbbe21c90c03051fdca063f927029a9a9c90
slzz <faceitff41@gmail.com>
2025-10-12 22:35:32 +0300

    Fix bugs with sonar

--- 6lab/src/bicycle.cpp
+++ 6lab/src/bicycle.cpp
@@ -1,10 +1,11 @@
 #include "../include/bicycle.h"
 
-#include <iostream>
 #include <format>
+#include <iostream>
 
 #include "../include/consts.h"
 #include "../include/invalidException.h"
+
 
 using namespace std;
 
@@ -14,20 +15,16 @@ Bicycle::Bicycle()
 double Bicycle::calculateCost(double distance, double weight) const {
     if (weight > BICYCLE_LOAD_CAP) {
         throw InvalidException(
-            std::format(
-                "Bicycle can't carry such a load! Capacity: {} kg, requested: {} kg",
-                static_cast<int>(BICYCLE_LOAD_CAP),
-                static_cast<int>(weight)
-            )
-        );
+            std::format("Bicycle can't carry such a load! Capacity: {} kg, requested: {} kg",
+                        static_cast<int>(BICYCLE_LOAD_CAP), static_cast<int>(weight)));
     }
     return distance * BICYCLE_COST_PER_KM * (1 + weight / BICYCLE_WEIGHT_FACTOR);
 }
 
 double Bicycle::calculatePassengerCost(double distance, int passengers) const {
     if (passengers > 1) {
-        throw InvalidException("Bicycle can only carry 1 passenger! Requested: " +
-                               to_string(passengers));
+        throw InvalidException(
+            std::format("Bicycle can only carry 1 passenger! Requested: {}", passengers));
     }
     return distance * BICYCLE_COST_PER_KM;
 }
--- 6lab/src/car.cpp
+++ 6lab/src/car.cpp
@@ -1,36 +1,32 @@
 #include "../include/car.h"
 
-#include <iostream>
 #include <format>
+#include <iostream>
 
 #include "../include/consts.h"
 #include "../include/invalidException.h"
-#include "../include/utils.h"
 #include "../include/transport.h"
+#include "../include/utils.h"
+
 
 using namespace std;
 
 Car::Car() : Transport(CAR_SPEED_IN_KM, CAR_COST_PER_KM, CAR_LOAD_CAP, CAR_REG_NUMBER) {}
 
-#include <format>
-
 double Car::calculateCost(double distance, double weight) const {
     if (weight > CAR_LOAD_CAP) {
         throw InvalidException(
-            std::format(
-                "Car can't carry such a load! Capacity: {} kg, requested: {} kg",
-                static_cast<int>(CAR_LOAD_CAP),
-                static_cast<int>(weight)
-            )
-        );
+            std::format("Car can't carry such a load! Capacity: {} kg, requested: {} kg",
+                        static_cast<int>(CAR_LOAD_CAP), static_cast<int>(weight)));
     }
     return distance * CAR_COST_PER_KM * (1 + weight / CAR_WEIGHT_FACTOR);
 }
 
 double Car::calculatePassengerCost(double distance, int passengers) const {
     if (passengers > CAR_MAX_PASS) {
-        throw InvalidException("Car can't carry that many passengers! Max: 5, requested: " +
-                               to_string(passengers));
+        throw InvalidException(
+            std::format("Car can't carry that many passengers! Max: {}, requested: {}",
+                        CAR_MAX_PASS, passengers));
     }
 
     return distance * CAR_COST_PER_KM * passengers;
